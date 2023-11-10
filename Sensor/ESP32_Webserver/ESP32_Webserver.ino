b6ad4a2f52eba7eef3ba1e3e10c3b5039a2046be
l-gebhard <lars.gebhard@smail.inf.h-brs.de>
2023-11-10 19:23:05 +0100

    Set up server

--- Sensor/ESP32_Webserver/ESP32_Webserver.ino
+++ Sensor/ESP32_Webserver/ESP32_Webserver.ino
@@ -1,6 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "WiFiCredentials.h"
+#include "ESPAsyncTCP.h"
+#include "ESPAsyncWebServer.h"
 
+AsyncWebServer server(80);
+
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWD;
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("WiFi Failed!\n");
+    return;
+  }
+
+  server.begin();
 }
 
 void loop() {
