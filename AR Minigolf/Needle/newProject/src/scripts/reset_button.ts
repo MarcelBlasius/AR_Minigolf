5d62158741f1e2afc9b714cb5940cbd14b1d3c42
Marcel <blasius.marcel@gmail.com>
2023-11-15 22:31:10 +0100

    fixed compile errors

--- AR Minigolf/Needle/newProject/src/scripts/reset_button.ts
+++ AR Minigolf/Needle/newProject/src/scripts/reset_button.ts
@@ -22,23 +22,11 @@ export class ResetButton extends Behaviour implements IPointerClickHandler {
         }
 
         this.object.visible = true;
-        const velo = this.body.getVelocity();
-        if (velo.x > 0 || velo.y > 0 || velo.z > 0){
-            console.log('can not reset a ball that is in movement');
-            return;
-        }
-
         console.log('Resetting ball');
 
         const refWorldPos = new Vector3();
         this.reference.getWorldPosition(refWorldPos);
         this.body?.teleport({ x: refWorldPos.x, y: refWorldPos.y + this.object.scale.y * 0.55, z: refWorldPos.z }, false)
-
-<<<<<<< HEAD
         this.body?.setVelocity(0, 0, 0);
-=======
-        this.object.setRotationFromEuler(this.reference.rotation);
-        await requestOwnership(this._sync);
->>>>>>> 0371a192f08d718baa36d03948fd782ba4d24328
     }
 }
