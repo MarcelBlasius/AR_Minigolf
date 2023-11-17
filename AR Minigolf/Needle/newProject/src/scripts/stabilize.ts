30e626ba096fe1fbc4f0b17da425d3a40b5389fd
Marcel <blasius.marcel@gmail.com>
2023-11-17 16:32:51 +0100

    stabilized image tracking

--- AR Minigolf/Needle/newProject/src/scripts/stabilize.ts
+++ AR Minigolf/Needle/newProject/src/scripts/stabilize.ts
@@ -3,8 +3,6 @@ import { Vector3 } from "three";
 
 export class Stabilize extends Behaviour {
     update(): void {
-        this.gameObject.rotateX(-this.gameObject.rotation.x)
-        this.gameObject.rotateZ(-this.gameObject.rotation.z);
     }
 }
 
