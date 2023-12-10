d7c8361b2c70ba3710133942efd6dcccf500964e
l-gebhard <lars.gebhard@smail.inf.h-brs.de>
2023-12-10 21:00:31 +0100

    Added additional events and buttons for direction

--- Sensor/ESP32 Webserver/data/script.js
+++ Sensor/ESP32 Webserver/data/script.js
@@ -1,40 +1,52 @@
 if (!!window.EventSource) {
-    var sourceSensor = new EventSource('/SensorReadings');
-    var sourceReset = new EventSource('/Reset');
+  var sourceSensor = new EventSource('/SensorReadings');
+  var sourceReset = new EventSource('/Reset');
+  var sourceDirection = new EventSource('/Direction');
 
-    sourceSensor.addEventListener('open', function(e) {
-      console.log("SensorReadings Connected");
-    }, false);
+  sourceSensor.addEventListener('open', function (e) {
+    console.log("SensorReadings Connected");
+  }, false);
 
-    sourceReset.addEventListener('open', function(e) {
-        console.log("Reset Connected");
-      }, false);
-  
-    sourceSensor.addEventListener('error', function(e) {
-      if (e.target.readyState != EventSource.OPEN) {
-        console.log("SensorReadings Disconnected");
-      }
-    }, false);
-  
-    sourceReset.addEventListener('error', function(e) {
-        if (e.target.readyState != EventSource.OPEN) {
-          console.log("Reset Disconnected");
-        }
-      }, false);
+  sourceReset.addEventListener('open', function (e) {
+    console.log("Reset Connected");
+  }, false);
 
-    sourceSensor.addEventListener('readings', function(e) {
-        console.log("readings", e.data);
-        var data = JSON.parse(e.data);
-        console.log(data);
-        document.getElementById('accX').innerHTML = data.accX;
-        document.getElementById('accY').innerHTML = data.accY;
-        document.getElementById('accZ').innerHTML = data.accZ;
-        document.getElementById('gyroX').innerHTML = data.gyroX;
-        document.getElementById('gyroY').innerHTML = data.gyroY;
-        document.getElementById('gyroZ').innerHTML = data.gyroZ;
-      }, false);
+  sourceDirection.addEventListener('open', function (e) {
+    console.log("Direction Connected");
+  }, false);
 
-      sourceReset.addEventListener('reset', function(e) {
-        console.log("reset", e.data);
-      }, false);
-  }
+  sourceSensor.addEventListener('error', function (e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("SensorReadings Disconnected");
+    }
+  }, false);
+
+  sourceReset.addEventListener('error', function (e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Reset Disconnected");
+    }
+  }, false);
+
+  sourceDirection.addEventListener('error', function (e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Direction Disconnected");
+    }
+  }, false);
+
+  sourceSensor.addEventListener('readings', function (e) {
+    console.log("readings", e.data);
+    var data = JSON.parse(e.data);
+    console.log(data);
+    document.getElementById('accX').innerHTML = data.accX;
+    document.getElementById('accY').innerHTML = data.accY;
+    document.getElementById('accZ').innerHTML = data.accZ;
+  }, false);
+
+  sourceReset.addEventListener('reset', function (e) {
+    console.log("reset", e.data);
+  }, false);
+
+  sourceDirection.addEventListener('direction', function (e) {
+    console.log("direction", e.data);
+  }, false);
+}
--- Sensor/ESP32 Webserver/src/main.cpp
+++ Sensor/ESP32 Webserver/src/main.cpp
@@ -11,6 +11,8 @@
 
 #define BUTTON_PIN_SENSOR 19
 #define BUTTON_PIN_RESET 18
+#define BUTTON_PIN_LEFT 17
+#define BUTTON_PIN_RIGHT 16
 #define DEBOUNCE_TIME 100
 #define MAX_SWING_TIME 10000
 #define MIN_ACC 10
@@ -19,54 +21,71 @@
 AsyncWebServer server(4200);
 AsyncEventSource sensorReadings("/SensorReadings");
 AsyncEventSource reset("/Reset");
+AsyncEventSource direction("/Direction");
 
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PASSWD;
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWD;
 
 Adafruit_MPU6050 sensor;
 sensors_event_t a, g, temp;
-float gyroX, gyroY, gyroZ;
-float accX, accY, accZ;
 JSONVar readings;
 
-int lastSteadyStateSensor = LOW;       
-int lastFlickerableStateSensor = LOW; 
-int currentStateSensor;                
+int lastSteadyStateSensor = LOW;
+int lastFlickerableStateSensor = LOW;
+int currentStateSensor;
 unsigned long lastDebounceTimeSensor = 0;
 
-int lastSteadyStateReset = LOW;       
-int lastFlickerableStateReset = LOW; 
-int currentStateReset;                
+int lastSteadyStateReset = LOW;
+int lastFlickerableStateReset = LOW;
+int currentStateReset;
 unsigned long lastDebounceTimeReset = 0;
 
-void initWiFi() {
+int lastSteadyStateLeft = LOW;
+int lastFlickerableStateLeft = LOW;
+int currentStateLeft;
+unsigned long lastDebounceTimeLeft = 0;
+
+int lastSteadyStateRight = LOW;
+int lastFlickerableStateRight = LOW;
+int currentStateRight;
+unsigned long lastDebounceTimeRight = 0;
+
+void initWiFi()
+{
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  if (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
     Serial.printf("WiFi Failed!\n");
     return;
   }
   Serial.println(WiFi.localIP());
 }
 
-void initSensor() {
-  if (!sensor.begin()) {
+void initSensor()
+{
+  if (!sensor.begin())
+  {
     Serial.println("Failed to find MPU6050 chip");
-    while (1) {
+    while (1)
+    {
       delay(10);
     }
   }
   Serial.println("MPU6050 Found!");
 }
 
-void initSPIFFS() {
-  if (!SPIFFS.begin()) {
+void initSPIFFS()
+{
+  if (!SPIFFS.begin())
+  {
     Serial.println("An error has occurred while mounting SPIFFS");
   }
   Serial.println("SPIFFS mounted successfully");
 }
 
-String getSensorReadings() {
+String getSensorReadings()
+{
   sensor.getEvent(&a, &g, &temp);
 
   float maxAccX = 0;
@@ -76,95 +95,150 @@ String getSensorReadings() {
   unsigned long starttime = millis();
   unsigned long endtime = starttime;
 
-  while((endtime - starttime) <= MAX_SWING_TIME) {
+  while ((endtime - starttime) <= MAX_SWING_TIME)
+  {
     sensor.getEvent(&a, &g, &temp);
 
-    if((a.acceleration.x < MIN_ACC && (endtime - starttime) >  EARLIST_SWING_FINISH) || (a.acceleration.y < MIN_ACC && (endtime - starttime) >  EARLIST_SWING_FINISH)) {
+    if ((a.acceleration.x < MIN_ACC && (endtime - starttime) > EARLIST_SWING_FINISH) || (a.acceleration.z < MIN_ACC && (endtime - starttime) > EARLIST_SWING_FINISH))
+    {
       break;
     }
 
-    if(abs(a.acceleration.x) > maxAccX) {
+    if (abs(a.acceleration.x) > maxAccX)
+    {
       maxAccX = abs(a.acceleration.x);
     }
 
-    if(abs(a.acceleration.y) > maxAccY) {
+    if (abs(a.acceleration.y) > maxAccY)
+    {
       maxAccY = abs(a.acceleration.y);
     }
 
-    if(abs(a.acceleration.z) > maxAccZ) {
+    if (abs(a.acceleration.z) > maxAccZ)
+    {
       maxAccZ = abs(a.acceleration.z);
     }
-    
+
     endtime = millis();
   }
-  
+
   readings["accX"] = String(maxAccX);
   readings["accY"] = String(maxAccY);
   readings["accZ"] = String(maxAccZ);
 
-  return JSON.stringify (readings);
+  return JSON.stringify(readings);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
   pinMode(BUTTON_PIN_SENSOR, INPUT_PULLUP);
   pinMode(BUTTON_PIN_RESET, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_LEFT, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_RIGHT, INPUT_PULLUP);
   initWiFi();
   initSPIFFS();
   initSensor();
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", "text/html");
-  });
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/index.html", "text/html"); });
   server.serveStatic("/", SPIFFS, "/");
 
-  sensorReadings.onConnect([](AsyncEventSourceClient *client){
+  sensorReadings.onConnect([](AsyncEventSourceClient *client)
+                           {
     if(client->lastId()){
       Serial.printf("Client reconnected. Last message ID that it got is: %u\n", client->lastId());
     }
-    client->send("Hello Client.", NULL, millis(), 10000);
-  });
+    client->send("Hello Client.", NULL, millis(), 10000); });
 
-  reset.onConnect([](AsyncEventSourceClient *client){
+  reset.onConnect([](AsyncEventSourceClient *client)
+                  {
     if(client->lastId()){
       Serial.printf("Client reconnected. Last message ID that it got is: %u\n", client->lastId());
     }
-    client->send("Hello Client.", NULL, millis(), 10000);
-  });
+    client->send("Hello Client.", NULL, millis(), 10000); });
 
+  direction.onConnect([](AsyncEventSourceClient *client)
+                      {
+    if(client->lastId()){
+      Serial.printf("Client reconnected. Last message ID that it got is: %u\n", client->lastId());
+    }
+    client->send("Hello Client.", NULL, millis(), 10000); });
   server.addHandler(&sensorReadings);
   server.addHandler(&reset);
+  server.addHandler(&direction);
   server.begin();
 }
 
-void loop() {
+void loop()
+{
 
-  currentStateSensor  = digitalRead(BUTTON_PIN_SENSOR);
-  currentStateReset  = digitalRead(BUTTON_PIN_RESET);
+  currentStateSensor = digitalRead(BUTTON_PIN_SENSOR);
+  currentStateReset = digitalRead(BUTTON_PIN_RESET);
+  currentStateLeft = digitalRead(BUTTON_PIN_LEFT);
+  currentStateRight = digitalRead(BUTTON_PIN_RIGHT);
 
-  if (currentStateSensor != lastFlickerableStateSensor) {
+  if (currentStateSensor != lastFlickerableStateSensor)
+  {
     lastDebounceTimeSensor = millis();
     lastFlickerableStateSensor = currentStateSensor;
   }
 
-  if (currentStateReset != lastFlickerableStateReset) {
+  if (currentStateReset != lastFlickerableStateReset)
+  {
     lastDebounceTimeReset = millis();
     lastFlickerableStateReset = currentStateReset;
   }
 
-  if ((millis() - lastDebounceTimeSensor) > DEBOUNCE_TIME) {
-   
-    if(lastSteadyStateSensor == HIGH && currentStateSensor == LOW) {
-      sensorReadings.send(getSensorReadings().c_str(),"readings",millis());
+  if (currentStateLeft != lastFlickerableStateLeft)
+  {
+    lastDebounceTimeLeft = millis();
+    lastFlickerableStateLeft = currentStateLeft;
+  }
+
+  if (currentStateRight != lastFlickerableStateRight)
+  {
+    lastDebounceTimeRight = millis();
+    lastFlickerableStateRight = currentStateRight;
+  }
+
+  if ((millis() - lastDebounceTimeSensor) > DEBOUNCE_TIME)
+  {
+
+    if (lastSteadyStateSensor == HIGH && currentStateSensor == LOW)
+    {
+      sensorReadings.send(getSensorReadings().c_str(), "readings", millis());
     }
     lastSteadyStateSensor = currentStateSensor;
   }
 
-  if ((millis() - lastDebounceTimeReset) > DEBOUNCE_TIME) {
-   
-    if(lastSteadyStateReset == HIGH && currentStateReset == LOW) {
-      reset.send("reset","reset",millis());
+  if ((millis() - lastDebounceTimeReset) > DEBOUNCE_TIME)
+  {
+
+    if (lastSteadyStateReset == HIGH && currentStateReset == LOW)
+    {
+      reset.send("reset", "reset", millis());
     }
     lastSteadyStateReset = currentStateReset;
+  }
+
+  if ((millis() - lastDebounceTimeLeft) > DEBOUNCE_TIME)
+  {
+
+    if (lastSteadyStateLeft == HIGH && currentStateLeft == LOW)
+    {
+      direction.send("left", "direction", millis());
+    }
+    lastSteadyStateLeft = currentStateLeft;
+  }
+
+  if ((millis() - lastDebounceTimeRight) > DEBOUNCE_TIME)
+  {
+
+    if (lastSteadyStateRight == HIGH && currentStateRight == LOW)
+    {
+      direction.send("right", "direction", millis());
+    }
+    lastSteadyStateRight = currentStateRight;
   }
 }
