686adae479547f2ee47a2571c4414f27016320d8
MarcelBlasius <marcel.blasius@inf.h-brs.de>
2024-03-24 17:02:50 +0100

    added house and rotator

---
+++ AR Minigolf/Assets/Needle/Components.codegen/SyncObjects.cs
@@ -0,0 +1,16 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class SyncObjects : UnityEngine.MonoBehaviour
+	{
+		public UnityEngine.GameObject @startRef;
+		public UnityEngine.GameObject @object;
+		public void start(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
